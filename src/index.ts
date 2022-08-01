896106498a4534c8ff278a1fbaf4b1e9a0847caf
Allan Cristian <alcristi.sto@gmail.com>
2022-08-01 20:41:14 -0300

    merge com Edit

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = new App();
 app.server.listen(app.port, () => {
-  console.log(`http://localhost:${app.port}`);
+  console.log(`https://back-gestao.herokuapp.com/:${app.port}`);
 });
